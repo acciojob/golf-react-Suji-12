b3f2ac4f96ba4c120992978b7d138e24535425f4
Suji-12 <23csec23sujithas@gmail.com>
2026-01-04 19:04:13 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,42 +1,59 @@
-import React, { Component, useState } from "react";
-import '../styles/App.css';
+import React, { Component } from "react";
+import "../styles/App.css";
 
 class App extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            renderBall: false,
-            posi : 0,
-            ballPosition: { left: "0px" }
-        };
-        this.renderChoice = this.renderBallOrButton.bind(this)
-        this.buttonClickHandler = this.buttonClickHandler.bind(this)
+  constructor(props) {
+    super(props);
+    this.state = {
+      renderBall: false,
+      left: 0,
     };
+  }
 
-    buttonClickHandler() {
-   
-   }
-    renderBallOrButton() {
-		if (this.state.renderBall) {
-		    return <div className="ball" style={this.state.ballPosition}></div>
-		} else {
-		    return <button onClick={this.buttonClickHandler} >Start</button>
-		}
-    }
+  buttonClickHandler = () => {
+    this.setState({ renderBall: true });
+  };
 
-    // bind ArrowRight keydown event
-    componentDidMount() {
-      
-    }
+  componentDidMount() {
+    document.addEventListener("keydown", this.handleKeyDown);
+  }
 
-    render() {
-        return (
-            <div className="playground">
-                {this.renderBallOrButton()}
-            </div>
-        )
+  componentWillUnmount() {
+    document.removeEventListener("keydown", this.handleKeyDown);
+  }
+
+  handleKeyDown = (e) => {
+    if (e.keyCode === 39 && this.state.renderBall) {
+      this.setState((prev) => ({
+        left: prev.left + 5,
+      }));
     }
+  };
+
+  renderBallOrButton() {
+    if (this.state.renderBall) {
+      return (
+        <div
+          className="ball"
+          style={{ position: "relative", left: this.state.left + "px" }}
+        ></div>
+      );
+    } else {
+      return (
+        <button className="start" onClick={this.buttonClickHandler}>
+          Start
+        </button>
+      );
+    }
+  }
+
+  render() {
+    return (
+      <div id="main">
+        {this.renderBallOrButton()}
+      </div>
+    );
+  }
 }
-
 
 export default App;
